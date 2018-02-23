8eca063b9bec4beefb844d18d9b5998c083e49b6
AYIDouble <alpaypython@gmail.com>
2018-02-23 21:43:53 +0100

    Init

---
+++ Run-Blockchain/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple_Blockchain;
+
+namespace Run_Blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the blockchain and add the genesis block
+            List<Block> blockchain = new List<Block>();
+            Block genesisBlock = createGenesisBlock();
+            blockchain.Add(genesisBlock);
+            Block previousBlock = blockchain[0];
+            
+            // How many blocks should we add to the chain
+            // after the genesis block
+            int num_of_blocks_to_add = 20;
+
+            // Add blocks to the chain
+            for (int i = 0; i < num_of_blocks_to_add; i++)
+            {
+                Block blockToAdd = previousBlock.nextBlock(previousBlock);
+                blockchain.Add(blockToAdd);
+                previousBlock = blockToAdd;
+                // Tell everyone about it!
+                Console.WriteLine("Block #" + blockToAdd.index + " has been added to the blockchain!");
+                Console.WriteLine("Hash: " + blockToAdd.hash);
+            }
+
+            Console.ReadLine();
+        }
+
+        // The first Block of the Blockchain
+        static Block createGenesisBlock()
+        {
+            return new Block(0, DateTime.Now, "Genesis Block", "0");
+        }
+
+    }
+}
---
+++ Simple-Blockchain/Block.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Blockchain
+{
+    public class Block
+    {
+        public int index;
+        public DateTime timestamp;
+        public string data;
+        public string previous_hash;
+        public string hash;
+
+        public Block()
+        {
+        }
+
+        // The Definition of a Block
+        public Block(int index, DateTime timestamp, string data, string previous_hash)
+        {
+            this.index = index;
+            this.timestamp = timestamp;
+            this.data = data;
+            this.previous_hash = previous_hash;
+            this.hash = hashBlockSha256();
+        }
+
+        // The Blockchain uses SHA-256
+        private string hashBlockSha256()
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(GetUniqueKey(16)));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+        // Method for Random Strings
+        // https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings-in-c
+        public static string GetUniqueKey(int maxSize)
+        {
+            char[] chars = new char[62];
+            chars =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[1];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetNonZeroBytes(data);
+                data = new byte[maxSize];
+                crypto.GetNonZeroBytes(data);
+            }
+            StringBuilder result = new StringBuilder(maxSize);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+
+        // After the Genesis Block is created, we can create more Blocks
+        public Block nextBlock(Block lastBlock)
+        {
+            index = lastBlock.index + 1;
+            timestamp = DateTime.Now;
+            data = "Hey! I'm block " + index;
+            hash = lastBlock.hash;
+            return new Block(index, DateTime.Now, data, hash);
+        }
+    }
+}
